e947832468937438bc9647e605aa7f06ddaf4668
Gabriel Dunne <gdunne@quilime.com>
2011-12-18 18:11:59 -0800

    Encoding RSS

--- rss.php
+++ rss.php
@@ -1,5 +1,5 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/">
+<rss version="2.0">
 <channel>
 
 	<title>clmpr.com <?php echo $endpoint ?></title>
@@ -9,10 +9,11 @@
 
 	<?php for($i = 0; $row = $q->fetch(); $i++ ): ?>
 	<item>
-	    <title><?php echo htmlentities($row['title']); ?></title>
+	    <title><?php echo urlencode($row['title']); ?></title>
 	    <link><?php echo $row['url'] ?></link>
+	    <link><?php echo htmlentities($row['description']) ?></link>
+	    <link><?php echo implode(" ", $row['tags']) ?></link>
 	    <pubDate><?php echo date('r', strtotime($row['date'])); ?></pubDate>
-
 	</item>	
 
 	<?php endfor; ?>
