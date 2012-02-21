0f324ab8cc9540d8f9a82fc224431ab529e0dc01
Gabriel Dunne <gdunne@quilime.com>
2012-02-21 00:03:40 -0800

    Updated Init to have ALLOW_SIGNUP

--- init.php
+++ init.php
@@ -2,22 +2,36 @@
 
 session_start();
 
+
 # timezone
 putenv('TZ=America/Los_Angeles');
+
 
 # include folders
 ini_set('include_path', dirname(realpath(__FILE__)));
 define('TMP_DIR', dirname(realpath(__FILE__)) .'/../tmp');
 
+
 # settings
 define('BASE_URL', 'example.com');
+$username_exceptions = array(
+	'tags',
+	'tag',
+	'about',
+	'help'
+);
+define('ALLOW_SIGNUP', true);
+
 
 # database
 define('DB_DSN',    "mysql:host=<mysql.hostname.com>;dbname=<dbnam>");
 define('DB_UNAME',  "<user>");
 define('DB_PW',     "<pass>");
 
+
 ini_set('display_errors', 1);
 error_reporting( E_ALL );
 
+
 include 'data.php';
+include 'output.php';
